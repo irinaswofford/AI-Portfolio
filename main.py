824104111efeca8492a6f52f46319ad43da5c7c0
irinaswofford <57734155+irinaswofford@users.noreply.github.com>
2025-05-31 21:17:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -89,14 +89,7 @@ state_schema = frozenset([
 ])
 
 graph = StateGraph(state_schema=state_schema)
-import streamlit as st
 
-def st_redirect_js(url: str):
-   st.markdown("""
-    <a href="https://mail.google.com/" target="_blank">
-        <button style="font-size:16px;padding:10px 20px;border:none;background-color:#4CAF50;color:white;border-radius:5px;">Login to Gmail</button>
-    </a>
-""", unsafe_allow_html=True)
 
 # Button to trigger redirect
 if st.button("Login to Gmail"):

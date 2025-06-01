ad1979ebb3fa89e9be23c706c16fe31a2f61da57
irinaswofford <57734155+irinaswofford@users.noreply.github.com>
2025-05-31 19:38:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,10 @@ from StreamlitGauth.google_auth import Google_auth
 redirect_uri = "http://localhost:8501"
 
 login = Google_auth(clientId=st.secrets.client_id, 
- clientSecret=st.secrets.client_secret,redirect_uri=st.secrets.redirect_uri
+ clientSecret=st.secrets.client_secret,
+redirect_uri=st.secrets.redirect_uri,
+login_button_text="Continue with Google",
+logout_button_text="Logout",
  )
 
 
